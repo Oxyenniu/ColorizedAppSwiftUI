da30915aff29ed33270029baef358d89aa2d50f6
Oxyenniu <oxyenniu@gmail.com>
2023-01-15 19:19:46 +0300

    change background

--- ColorizedAppSwiftUI/ContentView.swift
+++ ColorizedAppSwiftUI/ContentView.swift
@@ -11,20 +11,25 @@ struct ContentView: View {
     @State private var redSliderValue = 128.0
     @State private var greenSliderValue = 128.0
     @State private var blueSliderValue = 128.0
+    let backgroundGradient = LinearGradient(
+        gradient: Gradient(colors: [Color.red, Color.yellow, Color.blue]),
+        startPoint: .top, endPoint: .bottom)
     var body: some View {
-        VStack(spacing: 30) {
-            ColorizedView(
-                redValue: redSliderValue / 255,
-                greenValue: greenSliderValue / 255,
-                blueValue: blueSliderValue / 255
-            )
-            SliderSettings(value: $redSliderValue, sliderColor: .red)
-            SliderSettings(value: $greenSliderValue, sliderColor: .green)
-            SliderSettings(value: $blueSliderValue, sliderColor: .blue)
-            Spacer()
+        ZStack {
+            backgroundGradient.ignoresSafeArea()
+            VStack(spacing: 30) {
+                ColorizedView(
+                    redValue: redSliderValue / 255,
+                    greenValue: greenSliderValue / 255,
+                    blueValue: blueSliderValue / 255
+                )
+                SliderSettings(value: $redSliderValue, sliderColor: .red)
+                SliderSettings(value: $greenSliderValue, sliderColor: .green)
+                SliderSettings(value: $blueSliderValue, sliderColor: .blue)
+                Spacer()
+            }
+            .padding()
         }
-       
-        .padding()
     }
 }
 
